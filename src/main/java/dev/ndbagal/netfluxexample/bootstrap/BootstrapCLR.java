7cbb96511c52522cbe4747c17c9c001312f24bb2
Navnath Bagal <me@ndbagal.dev>
2021-04-02 22:35:05 +0530

    Initialized data with spring boot command line runner

---
+++ src/main/java/dev/ndbagal/netfluxexample/bootstrap/BootstrapCLR.java
@@ -0,0 +1,34 @@
+package dev.ndbagal.netfluxexample.bootstrap;
+
+import java.util.UUID;
+
+import org.springframework.boot.CommandLineRunner;
+
+import dev.ndbagal.netfluxexample.domain.Movie;
+import dev.ndbagal.netfluxexample.repositories.MovieRepository;
+import reactor.core.publisher.Flux;
+
+public class BootstrapCLR implements CommandLineRunner {
+
+  private final MovieRepository movieRepository;
+
+  @Override
+  public void run(String... args) throws Exception {
+
+    // Clear old data and populate new
+    movieRepository.deleteAll()
+        .thenMany(Flux
+            .just("Silence of the Lambdas", "Aeon Flux", "Enter the Mono<Void>", "The Fluxxinator",
+                "Back to the future", "Meet the fluxes", "Lord of the Fluxes")
+            .map(title -> new Movie(title, UUID.randomUUID().toString())).flatMap(movieRepository::save))
+        .subscribe(null, null, () -> {
+          movieRepository.findAll().subscribe(System.out::println);
+        });
+
+  }
+
+  public BootstrapCLR(MovieRepository movieRepository) {
+    this.movieRepository = movieRepository;
+  }
+
+}
