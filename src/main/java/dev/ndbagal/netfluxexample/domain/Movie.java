c8003c73afec428fa507fa5b1b8676cfe843a7df
Navnath Bagal <me@ndbagal.dev>
2021-04-02 22:10:16 +0530

    Created domain model

---
+++ src/main/java/dev/ndbagal/netfluxexample/domain/Movie.java
@@ -0,0 +1,19 @@
+package dev.ndbagal.netfluxexample.domain;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Document
+@Data
+@NoArgsConstructor
+public class Movie {
+
+  private String id;
+
+  @NonNull
+  private String title;
+
+}
