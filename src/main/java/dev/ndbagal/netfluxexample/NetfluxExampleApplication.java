a00c79ca886f6d929b98e473720d86bcb83f1d0c
Navnath Bagal <me@ndbagal.dev>
2021-04-02 22:02:19 +0530

    Initial commit

---
+++ src/main/java/dev/ndbagal/netfluxexample/NetfluxExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.ndbagal.netfluxexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfluxExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetfluxExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ndbagal/netfluxexample/NetfluxExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ndbagal.netfluxexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetfluxExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
